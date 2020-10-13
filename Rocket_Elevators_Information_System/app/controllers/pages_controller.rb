8b5aa392ce82d75aa653335d2c38ed8325535f3b
ArtificialSoda <dimitrov.fabien@gmail.com>
2020-10-13 17:11:50 -0400

    Change root path from home to index

--- Rocket_Elevators_Information_System/app/controllers/pages_controller.rb
+++ Rocket_Elevators_Information_System/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-  def home
-  end
-  
+  #def home
+  #end
+
   def index
   end
     
--- Rocket_Elevators_Information_System/config/routes.rb
+++ Rocket_Elevators_Information_System/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'pages#home'
+  root 'pages#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
 
 end
